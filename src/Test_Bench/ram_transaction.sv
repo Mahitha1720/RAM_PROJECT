d34b3c276d5c06fcaa3c918edf9854839d69fb8e
Mahitha <mahitha1727@gmail.com>
2026-07-07 01:33:28 +0530

    Add ram_transaction classes for read and write operations

---
+++ src/Test_Bench/ram_transaction.sv
@@ -0,0 +1,50 @@
+//`include "defines.svh"
+class ram_transaction;
+rand logic [`DATA_WIDTH-1:0] data_in;
+rand logic write_enb,read_enb;
+rand logic [ADDR_WIDTH-1:0] address;
+logic [`DATA_WIDTH-1:0] data_out;
+
+constraint wr_rd_constraint {{write_enb,read_enb} inside {[0:3]};}
+
+//constraint wr_not_equal_rd {{write_enb, read_enb} != 2'b11;}
+
+virtual function ram_transaction copy();
+copy = new();
+copy.data_in=this.data_in;
+copy.write_enb=this.write_enb;
+copy.read_enb=this.read_enb;
+copy.address=this.address;
+return copy;
+endfunction
+endclass
+
+class ram_transaction_write extends ram_transaction;
+
+constraint wr_rd_constraint {{write_enb,read_enb}==2'b10;}
+
+virtual function ram_transaction copy();
+ram_transaction_write copy1;
+copy1=new();
+copy1.data_in=this.data_in;
+copy1.write_enb=this.write_enb;
+copy1.read_enb=this.read_enb;
+copy1.address=this.address;
+return copy1;
+endfunction
+endclass
+class ram_transaction_read extends ram_transaction;
+
+constraint wr_rd_constraint {{write_enb,read_enb}==2'b01;}
+
+virtual function ram_transaction copy();
+ram_transaction_read copy2;
+copy2=new();
+copy2.data_in=this.data_in;
+copy2.write_enb=this.write_enb;
+copy2.read_enb=this.read_enb;
+copy2.address=this.address;
+return copy2;
+endfunction
+endclass
+
